05593dfd2742eacb77810650702b20b344b8107d
Robert Becket <36114621+bobbecket@users.noreply.github.com>
2021-10-11 11:34:25 -0700

    Initial commit
    Initial commit

--- js/site.js
+++ js/site.js
@@ -1,3 +1,38 @@
-function HelloWorld(){
-    alert("Hello World");
+// Controller
+//
+// Get the string from the page
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+// Logic
+//
+// Reverse the string
+function reverseString(userString) {
+
+    let revString = [];
+
+    let strLen = userString.length;
+
+    for (let index = strLen - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    return revString;
+}
+
+// View
+//
+// Display the reversed string to the user
+function displayString(revString) {
+
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
